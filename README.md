5b59dd0073fa95fb28c675186ee96d26877da275
seLain <selain@nature.ee.ncku.edu.tw>
2017-09-17 08:52:01 +0800

    add task model

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Arya
 Development of an experimental kanban system
+
+## Run
+
+Under your virtual environment for Arya :
+
+```
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver [IP:Port]
+```
+
+By default the server runs on http://localhost:8000
--- kanban/models.py
+++ kanban/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+
+import datetime
 
 # Create your models here.
+
+class Task(models.Model):
+	# 
+	title = models.CharField(max_length=128)
+	description = models.TextField(blank=True, null=True)
+	time_effort = models.PositiveSmallIntegerField(default=0)
+	# 
+	creator = models.ForeignKey(settings.AUTH_USER_MODEL)
+	create_date = models.DateTimeField(default=datetime.datetime.now)
+	lastupate_date = models.DateTimeField(default=datetime.datetime.now)
