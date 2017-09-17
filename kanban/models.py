d6a032adb5269a07bfb8b340f13a225c1a015deb
seLain <selain@nature.ee.ncku.edu.tw>
2017-09-17 17:17:05 +0800

    add project model

--- kanban/models.py
+++ kanban/models.py
@@ -5,11 +5,20 @@ import datetime
 
 # Create your models here.
 
+class Project(models.Model):
+	#
+	title = models.CharField(max_length=128, unique=True)
+	description = models.TextField(blank=True, null=True)
+	membres = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, null=True)
+	#
+	create_date = models.DateTimeField(default=datetime.datetime.now)
+
 class Task(models.Model):
 	# 
 	title = models.CharField(max_length=128)
 	description = models.TextField(blank=True, null=True)
 	time_effort = models.PositiveSmallIntegerField(default=0)
+	project = models.ForeignKey(Project, null=True)
 	# 
 	creator = models.ForeignKey(settings.AUTH_USER_MODEL)
 	create_date = models.DateTimeField(default=datetime.datetime.now)
--- kanban/views.py
+++ kanban/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Task
+from .models import Project, Task
 
 # Create your views here.
 
 def index(request):
 
-	# load all kanban tasks
-	all_tasks = Task.objects.all()
-	data = [task.title for task in all_tasks]
-	print(data)
+	# load all kanban projects
+	all_projects = Project.objects.all()
 
-	return render(request, 'kanban/index.html', {'tasks': data})
+	return render(request, 'kanban/index.html', {'projects': all_projects})
