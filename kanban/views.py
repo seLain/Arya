a337284b188653af7c7eafe2ef55a17839f54ed4
seLain <selain@nature.ee.ncku.edu.tw>
2017-09-18 22:13:31 +0800

    add command field in project page
    The command field supports only stage creation as POC. Much enhancement is needed.

--- kanban/views.py
+++ kanban/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from .models import Project, Stage, Task
 from .forms import TaskForm
 
+import json
+
 # Create your views here.
 
 def index(request):
@@ -36,3 +38,31 @@ def task_page(request):
 		return redirect('/kanban/project?id=%s' % task.project.id)
 
 	return render(request, 'kanban/task.html', {'task': task, 'form': form})
+
+def command(request):
+
+	command_text = request.POST['commandText']
+	# [ToDo] apply command dispatch pattern to deal with this part
+	response = command_dispatch(command_text, request.POST)
+
+	return HttpResponse(json.dumps(response), content_type="application/json") 
+
+def command_dispatch(command_text, request_post):
+
+	# [ToDo] using CLI command processing library to deal with this part
+	command_text = command_text.strip()
+	command_args = [arg for arg in command_text.split(' ') if arg != ' ']
+
+	response = {'update': False}
+	if len(command_args) == 3 :
+		if command_args[0].lower() == 'create' and command_args[1].lower() == 'stage':
+			project = Project.objects.get(id=request_post['projectID'])
+			largest_order = Stage.objects.filter(project=project).order_by('-order')[0].order
+			stage, created = Stage.objects.get_or_create(title=command_args[2], 
+														 project=project, 
+														 order=largest_order+1)
+			if created:
+				response['update'] = True
+				response['stage_title'] = stage.title
+
+	return response
