2883b039c3866d5fcf266429ab3f1655d41370bc
seLain <selain@nature.ee.ncku.edu.tw>
2017-09-22 00:33:08 +0800

    enable delete task by command

---
+++ kanban/command_bot/plugins/delete_task.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from ..bot import respond_to
+from kanban.models import Project, Stage, Task
+
+@respond_to('delete task id=#(.*)', re.IGNORECASE)
+def delete_task(message, task_id):
+
+	response = {'update': False}
+	
+	task =  Task.objects.get(id=task_id)
+	project = task.project
+	stage =task.stage
+	num, dict_del_type_num = Task.objects.get(id=task_id).delete()
+
+	if num > 0:
+		response['update'] = True
+		response['event'] = 'delete_task'
+		response['stage_title'] = stage.title
+		response['task_id'] = task_id
+
+	return response
