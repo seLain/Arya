2010071f646180fdc1b5a858aa705312e3a0e109
seLain <selain@nature.ee.ncku.edu.tw>
2018-02-28 01:32:39 +0800

    typo fix

--- kanban/models.py
+++ kanban/models.py
@@ -12,7 +12,7 @@ class Project(models.Model):
 	#
 	title = models.CharField(max_length=128, unique=True)
 	description = models.TextField(blank=True, null=True)
-	membres = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, null=True)
+	members = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, null=True)
 	#
 	create_date = models.DateTimeField(default=timezone.now)
 
