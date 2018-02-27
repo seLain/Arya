cb09789d8b137684f71c3fc6a8a6a22e1b371646
seLain <selain@nature.ee.ncku.edu.tw>
2018-02-28 01:34:51 +0800

    remove useless null=True parameters

--- kanban/models.py
+++ kanban/models.py
@@ -12,7 +12,7 @@ class Project(models.Model):
 	#
 	title = models.CharField(max_length=128, unique=True)
 	description = models.TextField(blank=True, null=True)
-	members = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, null=True)
+	members = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
 	#
 	create_date = models.DateTimeField(default=timezone.now)
 
